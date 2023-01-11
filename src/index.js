6896d9b0407dac47a25a2615be2fe617ae0eba82
Konstantsin Karpov <karpoffkos@mal.ru>
2023-01-11 18:10:06 +0300

    feat:sese

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let str499 = ""+num;
+    let arr500 = [];
+    for (let i = 0; str499.length > i; i++){
+      arr500.unshift(str499[i])
+    }
+     return (arr500.join("") * 1);
+  }
