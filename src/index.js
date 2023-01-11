62e91f606d3de7db9c95cc633d716fc596aa04ca
Kanstantsin Karpau <karpoffk92@gmail.com>
2023-01-12 00:21:33 +0300

    feat:dd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 module.exports = function reverse (n) {
-    let str499 = ""+num;
-    let arr500 = [];
-    for (let i = 0; str499.length > i; i++){
-      arr500.unshift(str499[i])
-    }
-     return (arr500.join("") * 1);
+    let str499 = ""+n;
+    let arr500 = "";
+    if(n !== NaN){
+    for (let i = 1; str499.length >= i; i++){
+      arr500+=str499[str499.length-i]
+    }}
+     return(parseInt(arr500));
   }
+  function reverse (n) {
+    let str499 = ""+n;
+    let arr500 = "";
+    if(n !== NaN){
+    for (let i = 1; str499.length >= i; i++){
+      arr500+=str499[str499.length-i]
+    }
+}
+     console.log(parseInt(arr500));
+  }
+reverse(-192);
+
